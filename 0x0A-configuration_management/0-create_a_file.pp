bcb221b23ff24c3e1ae614c1379e7da74f0213d1
tewa92 <tewachewsaleigizer@gmail.com>
2024-04-19 11:36:28 -0700

    puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+This manifest is about create a file at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
