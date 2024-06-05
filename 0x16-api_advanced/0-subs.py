94be0f6b1f7ff9d4eb9fddcbd7e4dc7a6a5b672e
tewa92 <tewachewsaleigizer@gmail.com>
2024-06-04 22:27:39 -0700

    Api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,14 +6,16 @@ This script queries subscribers on a given reddit subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    Queries the Reddit API and returns the number of subscribers for a given
+    subreddit.
     If the subreddit is invalid, returns 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/50.0"}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
