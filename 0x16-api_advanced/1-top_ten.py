10cf3f2df169b79386fae06a6a6c96538492b801
tewa92 <tewachewsaleigizer@gmail.com>
2024-06-04 23:53:34 -0700

    api_advanced

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that queries the Reddit API and prints the titles of the first 10 hot
+posts listed for a given subreddit.
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"][:10]:
+            print(post["data"]["title"])
+    else:
+        print("None")
