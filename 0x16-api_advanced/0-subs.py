074b26b5ee80d57603833411169dccfafe806eca
tewa92 <tewachewsaleigizer@gmail.com>
2024-06-05 00:15:12 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     If the subreddit is invalid, returns 0.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/50.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
