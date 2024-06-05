6e6ebe7a2971338854011497ab69682bfc8be6a3
tewa92 <tewachewsaleigizer@gmail.com>
2024-06-05 00:17:49 -0700

    api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-"""
+'''
 Script that queries the Reddit API and prints the titles of the first 10 hot
 posts listed for a given subreddit.
-"""
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """
+    '''
     Prints the titles of the first 10 hot posts listed for a given subreddit.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    '''
+    user = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=user, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"][:10]:
-            print(post["data"]["title"])
+        try:
+            data = response.json()
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        except (ValueError, KeyError) as e:
+            print(None)
     else:
-        print("None")
+        print(None)
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        top_ten(argv[1])
+    else:
+        print("Usage: {} <subreddit>".format(argv[0]))
