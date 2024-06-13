37fc208331b26b3a5b1921365796d205a2d2d8ee
tewa92 <tewachewsaleigizer@gmail.com>
2024-06-13 11:40:57 -0700

    git push

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# To fix nginx to accept and serve more requests.
+
+exec {'change max open files limit setting':
+  command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
